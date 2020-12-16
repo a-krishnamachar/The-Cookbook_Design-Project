ce3505bd3ba1ce1317bde85d7036037b60f4cc0c
faithletzkus <fletzkus@wustl.edu>
2020-12-16 15:31:30 -0600

    Search for recipes working

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -13,44 +13,49 @@ class Home extends React.Component {
       userMap: {},
     };
   }
+
+  componentDidMount() {
+    const userMap = {};
+    const users = this.props.firebase.users();
+    users.get().then((querySnapshot) => {
+      querySnapshot.forEach((userDoc) => {
+        let data = userDoc.data();
+        userMap[userDoc.id] = {
+          cookbook: data.cookbook,
+          email: data.email,
+          name: data.name,
+          friends: data.friends,
+        };
+      });
+      this.setState({ userMap: userMap });
+    });
+    const recipeList = [];
+    const recipes = this.props.firebase.recipes();
+    recipes.get().then((querySnapshot) => {
+      querySnapshot.forEach((userDoc) => {
+        let recipe = {};
+        var data = userDoc.data();
+        recipe.id = userDoc.id;
+        recipe.creatorId = data.creator;
+        recipe.creatorName = this.state.userMap[data.creator].name;
+        recipe.difficulty = data.difficulty;
+        recipe.ingredients = data.ingredients;
+        recipe.instructions = data.instructions;
+        recipe.time = data.time;
+        recipe.title = data.title;
+        recipe.image = data.image;
+        recipeList.push(recipe);
+      });
+      this.setState({ recipeList: recipeList });
+    });
+
+  }
+
   render() {
     return (
       <AuthUserContext.Consumer>
         {(authUser) => {
-          const userMap = {};
-          const users = this.props.firebase.users();
-          users.get().then((querySnapshot) => {
-            querySnapshot.forEach((userDoc) => {
-              let data = userDoc.data();
-              userMap[userDoc.id] = {
-                cookbook: data.cookbook,
-                email: data.email,
-                name: data.name,
-                friends: data.friends,
-              };
-            });
-            this.setState({ userMap: userMap });
-          });
-          const recipeList = [];
-          const recipes = this.props.firebase.recipes();
-          recipes.get().then((querySnapshot) => {
-            querySnapshot.forEach((userDoc) => {
-              let recipe = {};
-              var data = userDoc.data();
-              recipe.id = userDoc.id;
-              recipe.creatorId = data.creator;
-              recipe.creatorName = this.state.userMap[data.creator].name;
-              recipe.difficulty = data.difficulty;
-              recipe.ingredients = data.ingredients;
-              recipe.instructions = data.instructions;
-              recipe.time = data.time;
-              recipe.title = data.title;
-              recipe.image = data.image;
-              recipeList.push(recipe);
-            });
-            this.setState({ recipeList: recipeList });
-          });
-
+          console.log("in here")
           return (
             <div>
               <Header> Home </Header>
