b4686695978ea3da68ca60ef2bc8eca40344396f
Aditya Krishnamachar <adityakrishnamachar@gmail.com>
2020-12-16 11:24:56 -0700

    updated edit and cookbook files

--- src/components/MyCookbook/Cookbook.js
+++ src/components/MyCookbook/Cookbook.js
@@ -1,15 +1,40 @@
 import React from "react";
 import SignOut from "../SignOut/SignOut";
 import { Header } from "../../styles/styled";
+import { AddBtn } from "../../styles/styled";
+import { IconAlign } from "../../styles/styled";
 import { withAuthorization } from "../Session";
+
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from "@material-ui/core/InputAdornment";
+import IconButton from "@material-ui/core/IconButton";
+import TextField from "@material-ui/core/TextField";
+
 
 class MyCookbook extends React.Component {
   render() {
     return (
       <div>
-        <Header> My Cookbook </Header>
-        <SignOut />
+        <Header> My Cookbook <SignOut/></Header>
+
+      <TextField
+        placeholder={"Search your collections..."}
+        onChange={(e)=>this.searchSpace(e)}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment>
+              <IconButton>
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          )
+        }}
+      />
+      <AddBtn><button>+</button>
+      </AddBtn>
       </div>
+
+      //then call the recipe cards to show up below
     );
   }
 }
