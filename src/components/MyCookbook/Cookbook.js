ce075f1dee9e4f5a60e493e0f25f8d0089ddbbde
zhongyanjie <824009130@qq.com>
2020-12-16 19:59:03 -0600

    Fixed the search problems in the cookbook page

--- src/components/MyCookbook/Cookbook.js
+++ src/components/MyCookbook/Cookbook.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { CookbookCard } from "../Card/CookbookCard";
+import { Card } from "../Card/Card";
 import AddButton from "./AddButton";
 import { Header, CardAlign } from "../../styles/styled";
 import { compose } from "recompose";
 import { AuthUserContext, withAuthorization } from "../Session";
 import { withFirebase } from "../Firebase";
 import SignOut from "../SignOut/SignOut";
-import { AddBtn, SearchBoxAlign } from "../../styles/styled";
+import { SearchBoxAlign, PageCardAlign } from "../../styles/styled";
 import SearchIcon from "@material-ui/icons/Search";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import IconButton from "@material-ui/core/IconButton";
@@ -17,9 +18,16 @@ class MyCookbook extends React.Component {
     super(props);
     this.state = {
       recipeList: [],
+      allRecipes: [],
       userMap: {},
+      search: null,
     };
   }
+
+  searchSpace = (event) => {
+    let keyword = event.target.value;
+    this.setState({ search: keyword });
+  };
 
   componentDidMount() {
     const userMap = {};
@@ -37,25 +45,27 @@ class MyCookbook extends React.Component {
       this.setState({ userMap: userMap });
     });
     const recipeList = [];
+    const allRecipes = [];
     const recipes = this.props.firebase.recipes();
     recipes.get().then((querySnapshot) => {
       querySnapshot.forEach((userDoc) => {
         let recipe = {};
         var data = userDoc.data();
+        recipe.id = userDoc.id;
+        recipe.creatorId = data.creator;
+        recipe.creatorName = this.state.userMap[data.creator].name;
+        recipe.difficulty = data.difficulty;
+        recipe.ingredients = data.ingredients;
+        recipe.instructions = data.instructions;
+        recipe.time = data.time;
+        recipe.title = data.title;
+        recipe.image = data.image;
+        allRecipes.push(recipe);
         if (data.creator == this.props.firebase.currentUserId()) {
-          recipe.id = userDoc.id;
-          recipe.creatorId = data.creator;
-          recipe.creatorName = this.state.userMap[data.creator].name;
-          recipe.difficulty = data.difficulty;
-          recipe.ingredients = data.ingredients;
-          recipe.instructions = data.instructions;
-          recipe.time = data.time;
-          recipe.title = data.title;
-          recipe.image = data.image;
           recipeList.push(recipe);
         }
       });
-      this.setState({ recipeList: recipeList });
+      this.setState({ recipeList: recipeList, allRecipes: allRecipes });
     });
   }
 
@@ -63,6 +73,58 @@ class MyCookbook extends React.Component {
     return (
       <AuthUserContext.Consumer>
         {(authUser) => {
+          let originalStatus = true;
+          const allRecipeCards = this.state.allRecipes
+            .filter((recipe) => {
+              console.log("inidiv recipe", recipe);
+              console.log("this.state.search", this.state.search);
+              if (this.state.search == null || this.state.search == "") {
+                if (recipe.creatorId == this.props.firebase.currentUserId()) {
+                  originalStatus = true;
+                  return recipe;
+                }
+              }
+              // if nothing is currently in searchbar, return everything
+              else if (
+                recipe.title
+                  .toLowerCase()
+                  .includes(this.state.search.toLowerCase()) || //search for dish title
+                recipe.creatorName
+                  .toLowerCase()
+                  .includes(this.state.search.toLowerCase()) //search for dish creator
+              ) {
+                originalStatus = false;
+                return recipe;
+              } else {
+                // console.log("ingredients", recipe.ingredients)
+                for (const ingredient of Object.values(recipe.ingredients)) {
+                  if (ingredient != "undefined") {
+                    if (
+                      ingredient.ingredient.food
+                        .toLowerCase()
+                        .includes(this.state.search.toLowerCase())
+                    ) {
+                      //search for dish ingredient
+                      originalStatus = false;
+                      return recipe;
+                    }
+                  }
+                }
+              }
+            })
+            .map(
+              (recipe) =>
+                (originalStatus && (
+                  <CardAlign>
+                    <CookbookCard recipe={recipe} key={`${recipe.id}`} />
+                  </CardAlign>
+                )) ||
+                (!originalStatus && (
+                  <CardAlign>
+                    <Card recipe={recipe} key={`${recipe.id}`} />
+                  </CardAlign>
+                ))
+            );
           return (
             <div>
               <Header> My Cookbook</Header>
@@ -82,11 +144,7 @@ class MyCookbook extends React.Component {
                 />
                 <AddButton labelName="+" />
               </SearchBoxAlign>
-              <CardAlign>
-                {this.state.recipeList.map((recipe) => (
-                  <CookbookCard recipe={recipe} key={`${recipe.id}`} />
-                ))}
-              </CardAlign>
+              <PageCardAlign>{allRecipeCards}</PageCardAlign>
               <SignOut />
             </div>
           );
